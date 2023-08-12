f25bb00fcc3b1318a774ccd0b56a37e8d58bac50
losgab <tntrox.jr@gmail.com>
2023-08-13 02:34:08 +1000

    esp i2c handling code added

--- esp32-fdc1004/include/i2c_handler.h
+++ esp32-fdc1004/include/i2c_handler.h
@@ -1,7 +1,8 @@
 #include <driver/i2c.h>
 
-#define I2C_MASTER_SCL_IO 22
-#define I2C_MASTER_SDA_IO 21
-#define I2C_MASTER_FREQ_HZ 100000 
+#define I2C_MASTER_SCL_IO   GPIO_NUM_22
+#define I2C_MASTER_SDA_IO   GPIO_NUM_21
+#define I2C_MASTER_FREQ_HZ  100000
+#define I2C_MASTER_NUM      I2C_NUM_0
 
-esp_err_t i2c_master_init(void);
+esp_err_t i2c_master_init(int);
--- esp32-fdc1004/src/i2c_handler.c
+++ esp32-fdc1004/src/i2c_handler.c
@@ -2,7 +2,7 @@
 
 int i2c_master_port = 0;
 
-esp_err_t i2c_master_init(void)
+esp_err_t i2c_master_init(int i2c_master_channel)
 {
     i2c_config_t config = {
         .mode = I2C_MODE_MASTER,
@@ -12,5 +12,23 @@ esp_err_t i2c_master_init(void)
         .scl_pullup_en = GPIO_PULLUP_DISABLE,
         .master.clk_speed = I2C_MASTER_FREQ_HZ, // select frequency specific to your project
     };
-    // esp_err_t = i2c_param_config();
+    i2c_param_config(i2c_master_channel, &config);
+    i2c_driver_install(i2c_master_channel, config.mode, 0, 0, 0);
+}
+
+// Include a slave init function
+esp_err_t write_i2c_byte(i2c_port_t i2c_port, uint8_t data)
+{
+    void *link = i2c_cmd_link_create();
+    i2c_master_start(&link);
+
+    i2c_master_write_byte(&link, data, true);
+    
+    i2c_master_write(&link, data, 1, true);
+
+    i2c_master_stop(&link);
+
+    i2c_master_cmd_begin(i2c_port, &link, 100);
+
+    i2c_cmd_link_delete(&link);
 }
