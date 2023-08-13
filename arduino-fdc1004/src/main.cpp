d29ebcf1f2c2d16d52e944140752f3762f564cae
losgab <53280648+losgab@users.noreply.github.com>
2023-08-13 20:05:14 +1000

    loggin added for processing

--- arduino-fdc1004/src/main.cpp
+++ arduino-fdc1004/src/main.cpp
@@ -35,18 +35,17 @@ void loop()
 	measure(FDC, ENV_CHANNEL, FDC1004_400HZ, &capdac2);
 	float env_val = getMeasurement(FDC, ENV_CHANNEL, &capdac2);
 
-	Serial.print("Ref Sensor: ");
-	Serial.print(ref_val, 2);
-	Serial.println("  pf");
-	Serial.print("Level Sensor: ");
-	Serial.print(lev_val, 2);
-	Serial.println("  pf");
-	Serial.print("Env Sensor: ");
-	Serial.print(env_val, 2);
-	Serial.println("  pf");
+	Serial.println("----------");
+	Serial.print("R: ");
+	Serial.println(ref_val, 2);
+	// Serial.println("  pf");
+	Serial.print("L: ");
+	Serial.println(lev_val, 2);
+	// Serial.println("  pf");
+	Serial.print("E: ");
+	Serial.println(env_val, 2);
+	// Serial.println("  pf");
+	Serial.println("----------");
 
-	Serial.println("--------------");
-	// calculateLevel(ref_val, level_val, env_val / 4);
-	Serial.println("--------------");
 	delay(500);
 }
